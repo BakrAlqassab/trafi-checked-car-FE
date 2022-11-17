b3627ebc55f91155ecc1dc2b288c80bf6214aaba
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2022-11-17 23:00:04 +0200

    add the search functionality && show table data

---
+++ script.js
@@ -0,0 +1,88 @@
+document.getElementById("upload").onclick = function () {
+  var jsonFiles = document.getElementById("selectJsonFile").files;
+  console.log(jsonFiles);
+  if (jsonFiles.length <= 0) {
+    return false;
+  }
+
+  var fr = new FileReader();
+  fr.onload = function (e) {
+    console.log(e);
+
+    var result = JSON.parse(e.target.result);
+    const formattedData = JSON.stringify(result, null, 2);
+    axios
+      .post("http://localhost:3000/posts/", result)
+      .then((response) => {
+        this.fetchData()
+        const addedUser = response.data;
+        console.log(`POST: user is added`, addedUser);
+      })
+      .catch((error) => console.log(error));
+    console.log(formattedData);
+  };
+  fr.readAsText(jsonFiles.item(0));
+};
+
+const fetchData = () => {
+    let tblBody = document.getElementsByTagName("tbody");
+  axios
+    .get("http://localhost:3000/posts/")
+    .then((response) => {
+       const carsData = response.data;
+      // cells creation
+      var k ;
+
+
+// //end of the row
+console.log(carsData.length)
+      for (var i = 1; i < carsData.length; i++) {
+        // table row creation
+          k += '<tr>';
+          k += '<td>' + carsData[i].id  + '</td>';
+          k += '<td>' + carsData[i].model_year  + '</td>';
+          k += '<td>' + carsData[i].make + '</td>';
+          k += '<td>' + carsData[i].model + '</td>';
+          k += '<td>' + carsData[i].rejection_percentage + ' %'+ '</td>';
+          k += '<td>' + carsData[i].reason_1 + '</td>';
+          k += '<td>' + carsData[i].reason_2 + '</td>';
+          k += '<td>' + carsData[i].reason_3 + '</td>';
+          k += '</tr>';    
+      }
+      
+      document.getElementById("table-tbody").innerHTML=k; 
+    })
+    .catch((error) => console.error(error));
+};
+
+fetchData();
+
+function doSearch() {
+       // Declare variables 
+       var input, filter, table, tr, i, j, column_length, count_td;
+       
+       column_length = document.getElementById('table').rows[0].cells.length;
+   
+       input = document.getElementById("searchData");
+       filter = input.value.toUpperCase();
+       table = document.getElementById("table");
+       tr = table.getElementsByTagName("tr");
+       for (i = 1; i < tr.length; i++) { // except first(heading) row
+         count_td = 0;
+         for(j = 1; j < column_length-1; j++){ // except first column
+             td = tr[i].getElementsByTagName("td")[j];
+             /* ADD columns here that you want you to filter to be used on */
+             if (td) {
+               if ( td.innerHTML.toUpperCase().indexOf(filter) > -1)  {            
+                 count_td++;
+               }
+             }
+         }
+         if(count_td > 0){
+             tr[i].style.display = "";
+         } else {
+             tr[i].style.display = "none";
+         }
+       }
+
+}
