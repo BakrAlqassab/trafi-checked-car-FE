17de6001828b08c66e6904e007fafb6ee05539e9
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2022-11-18 15:04:10 +0200

    add more style and filter way functionality

--- script.js
+++ script.js
@@ -33,31 +33,32 @@ const fetchData = () => {
       let noContentSectio = document.getElementById("no-content");
       let tabelContentSection = document.getElementById("content-table");
       if (!carsData.length) {
-       
         noContentSectio.style.display = "flex";
-   
+
         tabelContentSection.style.display = "none";
       } else {
         noContentSectio.style.display = "none";
         // cells creation
-        var k;
-        for (var i = 0; i < carsData.length; i++) {
-          // table row creation
-          k += "<tr>";
-          k += "<td>" + carsData[i].id + "</td>";
-          k += "<td>" + carsData[i].model_year + "</td>";
-          k += "<td>" + carsData[i].make + "</td>";
-          k += "<td>" + carsData[i].model + "</td>";
-          k += "<td>" + carsData[i].rejection_percentage + " %" + "</td>";
-          k += "<td>" + carsData[i].reason_1 + "</td>";
-          k += "<td>" + carsData[i].reason_2 + "</td>";
-          k += "<td>" + carsData[i].reason_3 + "</td>";
-          k += "</tr>";
-        }
-        console.log(carsData[0]);
-        if (k.length) {
-          document.getElementById("table-tbody").innerHTML = k;
-        }
+
+        var tableBody = function () {
+          var rows = "";
+          for (var i = 0; i < carsData.length; i++) {
+            // table row creation
+            rows += "<tr>";
+            rows += "<td>" + carsData[i].id + "</td>";
+            rows += "<td>" + carsData[i].model_year + "</td>";
+            rows += "<td>" + carsData[i].make + "</td>";
+            rows += "<td>" + carsData[i].model + "</td>";
+            rows += "<td>" + carsData[i].rejection_percentage + " %" + "</td>";
+            rows += "<td>" + carsData[i].reason_1 + "</td>";
+            rows += "<td>" + carsData[i].reason_2 + "</td>";
+            rows += "<td>" + carsData[i].reason_3 + "</td>";
+            rows += "</tr>";
+          }
+          return rows;
+        };
+
+        document.getElementById("table-tbody").innerHTML = tableBody();
       }
     })
     .catch((error) => console.error(error));
@@ -65,7 +66,48 @@ const fetchData = () => {
 
 fetchData();
 
+// First filter way, can search in first and second column only in parallel and with max 50 available filtered data amount
+
 function doSearch() {
+  var input, filter, table, tr, i, j, column_length, count_td, filteredAmount = 0;
+  input = document.getElementById("searchData");
+  filter = input.value.toUpperCase();
+  // split the entered search input based on space to filter the value value1 search for Make cell, Value 2 = search for Model cell
+  filter = filter.split(" ");
+  table = document.getElementById("content-table");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    td2 = tr[i].getElementsByTagName("td")[3];
+    if (td && td2) {
+      txtValue = td.textContent || td.innerText;
+      txtValue2 = td2.textContent || td2.innerText;
+      if (txtValue.toUpperCase().indexOf(filter[0]) > -1) {
+        if (filter[1]) {
+          if (txtValue2.toUpperCase().indexOf(filter[1]) > -1 && filteredAmount < 50) {
+            filteredAmount++
+            console.log('called is')
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        } else if (!filter[1] && filteredAmount < 50) {
+            console.log('called not')
+            filteredAmount++
+          tr[i].style.display = "";
+        }
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+
+// Second filter way, can search in any columns, with 50 max data amount
+
+function doSearch1() {
   // Declare variables
   var input, filter, table, tr, i, j, column_length, count_td;
 
@@ -75,6 +117,7 @@ function doSearch() {
   filter = input.value.toUpperCase();
   table = document.getElementById("content-table");
   tr = table.getElementsByTagName("tr");
+  let r = 0;
   for (i = 1; i < tr.length; i++) {
     // except first(heading) row
     count_td = 0;
@@ -88,7 +131,8 @@ function doSearch() {
         }
       }
     }
-    if (count_td > 0) {
+    if (count_td > 0 && (r < 50 || filter === "")) {
+      r++;
       tr[i].style.display = "";
     } else {
       tr[i].style.display = "none";
